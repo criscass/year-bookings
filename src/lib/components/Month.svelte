49129daddfbeeb41ea7637e50c5a0bff3400f2e0
Cristian Cassetta <cristiancassetta@gmail.com>
2023-03-15 06:12:05 +0100

    Made some styling changes

--- src/lib/components/Month.svelte
+++ src/lib/components/Month.svelte
@@ -21,57 +21,62 @@
 	let current = calendarize(new Date(year, month), offset);
 	let next = calendarize(new Date(year, month + 1), offset);
 
-	function toPrev() {
-		[current, next] = [prev, current];
+	//  **********************************************************
 
-		if (--month < 0) {
-			month = 11;
-			year--;
-		}
+	// Functions for previous or next month buttons
+	// function toPrev() {
+	// 	[current, next] = [prev, current];
 
-		prev = calendarize(new Date(year, month - 1), offset);
-	}
+	// 	if (--month < 0) {
+	// 		month = 11;
+	// 		year--;
+	// 	}
 
-	function toNext() {
-		[prev, current] = [current, next];
+	// 	prev = calendarize(new Date(year, month - 1), offset);
+	// }
 
-		if (++month > 11) {
-			month = 0;
-			year++;
-		}
+	// function toNext() {
+	// 	[prev, current] = [current, next];
 
-		next = calendarize(new Date(year, month + 1), offset);
-	}
+	// 	if (++month > 11) {
+	// 		month = 0;
+	// 		year++;
+	// 	}
+
+	// 	next = calendarize(new Date(year, month + 1), offset);
+	// }
+	//  **********************************************************
+
 	// Returns true if year, month and day corrisponds to todays date
-	function isToday(day) {
+	function isToday(day: number) {
 		return today && today_year === year && today_month === month && today_day === day;
 	}
 </script>
 
-<div>
-	<header>
-		<button on:click={toPrev}><span> &#60 </span> </button>
-		<h4>{months[month]} {year}</h4>
-		<button on:click={toNext}><span> &#62 </span></button>
+<div class="card variant-soft pb-4 pt-2 px-8">
+	<header class="card-header pt-2 mb-2">
+		<h4 class="unstyled text-2xl">{months[month]}</h4>
 	</header>
 
-	{#each labels as txt, idx (txt)}
-		<span>{labels[(idx + offset) % 7]}</span>
-	{/each}
+	<section class="grid grid-cols-7 gap-4 text-lg text-center">
+		{#each labels as txt, idx (txt)}
+			<span>{labels[(idx + offset) % 7]}</span>
+		{/each}
 
-	{#each { length: 6 } as w, idxw (idxw)}
-		{#if current[idxw]}
-			{#each { length: 7 } as d, idxd (idxd)}
-				{#if current[idxw][idxd] != 0}
-					<span>
-						{current[idxw][idxd]}
-					</span>
-				{:else if idxw < 1}
-					<span>{prev[prev.length - 1][idxd]}</span>
-				{:else}
-					<span>{next[0][idxd]}</span>
-				{/if}
-			{/each}
-		{/if}
-	{/each}
+		{#each { length: 6 } as w, idxw (idxw)}
+			{#if current[idxw]}
+				{#each { length: 7 } as d, idxd (idxd)}
+					{#if current[idxw][idxd] != 0}
+						<span>
+							{current[idxw][idxd]}
+						</span>
+					{:else if idxw < 1}
+						<span>{prev[prev.length - 1][idxd]}</span>
+					{:else}
+						<span>{next[0][idxd]}</span>
+					{/if}
+				{/each}
+			{/if}
+		{/each}
+	</section>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
 	import Month from '$lib/components/Month.svelte';
 	const today = new Date();
+	export let year = 2023;
 </script>
 
-<Month {today} />
+<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3">
+	{#each { length: 12 } as x, idx (idx)}
+		<Month {today} month={idx} {year} />
+	{/each}
+</div>
