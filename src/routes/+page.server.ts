bffb65ff522dec209b781b68ac18c37970b35dbc
Cristian Cassetta <cristiancassetta@gmail.com>
2024-02-18 18:49:21 +0100

    Removed all old local database stuff

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,5 +1,4 @@
-import { getBookings, addBooking } from '../lib/server/database';
-import type { Actions, PageServerLoad } from './$types';
+import type { PageServerLoad } from './$types';
 import { redirect } from '@sveltejs/kit';
 
 export const load: PageServerLoad = async (event) => {
@@ -9,28 +8,5 @@ export const load: PageServerLoad = async (event) => {
 	}
 	if (!session) {
 		throw redirect(302, '/login');
-	}
-	const bookings = getBookings();
-	return { bookings: bookings };
-};
-
-export const actions: Actions = {
-	default: async ({ request }) => {
-		const data = await request.formData();
-		const bookingName = String(data.get('bookingName'));
-		const checkInDate = String(data.get('check-in'));
-		const checkOutDate = String(data.get('check-out'));
-		const bookingColor = String(data.get('bookingColor'));
-		const newBooking: Booking = {
-			id: crypto.randomUUID(),
-			name: bookingName,
-			start_on_day: new Date(checkInDate),
-			end_on_day: new Date(checkOutDate),
-			color: bookingColor
-		};
-
-		addBooking(newBooking);
-
-		return { success: true };
 	}
 };
